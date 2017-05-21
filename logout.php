f6fdf8e6107f8cd205afe6798ad3a0cfd51708c7
Billy Everyteen <bayaz@sabanciuniv.edu>
2017-05-21 14:20:19 +0300

    Session problem has been fixed

--- logout.php
+++ logout.php
@@ -2,6 +2,11 @@
 
 session_start();
 
+if ($_SERVER['REQUEST_METHOD'] != 'POST' || !isset($_SESSION['user_id'])) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    die();
+}
+
 session_unset();
 
 session_destroy();
